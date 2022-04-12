177ebb293e11eb371d1fee13672c0f2f44180a6f
Bình An <anldbps21776@fpt.edu.vn>
2022-04-13 00:47:19 +0700

    Update Gallery.js

--- Gallery/Gallery.js
+++ Gallery/Gallery.js
@@ -4,35 +4,10 @@ var index = 0;
 function loadAnh(){
     for(var i = 0; i < n ; i++){
         arrAnh[i] = new Image();
-        arrAnh[i].src = "/images/"+i+".png";
+        arrAnh[i].src = "https://anlavn.github.io/ASM-Web1042/images/"+i+".png";
     }
 }
-// function Next(){
-//     if(index < arrAnh.length - 1){
-//         index++;
-//         document.getElementById("Anh").src = arrAnh[index].src;
-//         document.getElementById("Back").disabled = false;
-//     }
-//     if(index == arrAnh.length - 1){document.getElementById("Next").disabled = true;}
-// }
-// function Back(){
-//     if(index > 0){
-//         index--;
-//         document.getElementById("Anh").src = arrAnh[index].src;
-//         document.getElementById("Next").disabled = false;
-//     }
-//     if(index == 0){document.getElementById("Back").disabled = true;}
-// }
-// function First(){
-//     index = 0;
-//     document.getElementById("Anh").src = arrAnh[index].src;
-//     Back();
-// }
-// function Last(){
-//     index = arrAnh.length - 1;
-//     document.getElementById("Anh").src = arrAnh[index].src;
-//     Next();
-// }
+
 function Next(){
     if(index < arrAnh.length - 1){index++;}
     else{index = 0;}
