6cf3c88c6bb042608fec9ce8d22fdd8390382cfd
Bình An <anldbps21776@fpt.edu.vn>
2022-04-14 15:42:16 +0700

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,17 +5,11 @@ function loadAnh(){
     for(var i = 0; i < n ; i++){
         arrAnh[i] = new Image();
         arrAnh[i].src = "https://anlavn.github.io/ASM-Web1042/images/"+i+".png";
-    }
-}
+    }}
 function Next(){
     if(index < arrAnh.length - 1){index++;}
     else{index = 0;}
     document.getElementById("Anh").src = arrAnh[index].src;
     console.log(document.getElementById("Anh").src);
-}
-function Back(){
-    if(index > 0){index--;}
-    else{index = arrAnh.length - 1;}
-    document.getElementById("Anh").src = arrAnh[index].src;
 }
 setInterval("Next()", 5000);
